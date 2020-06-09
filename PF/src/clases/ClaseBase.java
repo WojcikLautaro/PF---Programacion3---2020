8da88e286b9d4927eb72a438200c4ddc39c2e033
jadeDemon <56654296+jadeDemon@users.noreply.github.com>
2020-06-09 03:54:30 -0300

    a

--- PF/src/clases/ClaseBase.java
+++ PF/src/clases/ClaseBase.java
@@ -4,9 +4,10 @@ import java.util.ArrayList;
 import atributes.AtributeMultipliers;
 import habilidades.Habilidad;
 import razas.ClasesNoConcretas.RazaBase.POSIBLES_CLASES;
+import razas.InterfasesClasesNoConcretas.HerenciaParaTodasLasRazas;
 import recursos.Recursos;
 
-public abstract class ClaseBase {
+public abstract class ClaseBase implements HerenciaParaTodasLasRazas{
 	public abstract POSIBLES_CLASES getEnumDeClase ();
 	public abstract ArrayList<Habilidad> getClassAbilities ();
 	public abstract Recursos getClassResources ();
